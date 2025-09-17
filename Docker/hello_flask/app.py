51a4abf3491b9ae042fcc0138d7e99e30de03807
hashim1sharif <haashimsh@hotmail.com>
2025-09-17 15:39:26 +0200

    Modified app.py and deleted docker-compose.yml

--- Docker/hello_flask/app.py
+++ Docker/hello_flask/app.py
@@ -1,4 +1,3 @@
-
 # app.py
 
 from flask import Flask
@@ -21,13 +20,4 @@ def hello_world():
     return f'Hello, World! MySQL version: {version[0]}'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, world!'
-
-if  __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
